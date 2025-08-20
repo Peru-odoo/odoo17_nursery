9651627578f27fa1cad6793f8babefea6f90dfdc
Baselamr14 <bassel.odoo18@gmail.com>
2025-08-20 13:58:58 +0300

    Restore and add secondary_uom_all module at repo root (cleaned)

--- custom_addons/negative_stock/__init__.py
+++
@@ -1,1 +0,0 @@
-from . import models  # Import the "models" folder so Odoo loads them
--- custom_addons/negative_stock/models/__init__.py
+++
@@ -1,2 +0,0 @@
-from . import stock_config_settings  # Loads settings model
-from . import stock_picking  # Loads picking validation logic
