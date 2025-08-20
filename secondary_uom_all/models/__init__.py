f6e3f771a60d780d498cc5c3b1268658179451b6
baselamr14 <bassel.odoo18@gmail.com>
2025-08-20 13:48:41 +0300

    Delete secondary_uom_all directory

--- secondary_uom_all/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from . import product_template
-from . import sale_order_line
-from . import purchase_order_line
-from . import stock_move
-from . import account_move_line
-from . import stock_move_line
